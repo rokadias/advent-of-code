b1fe200816dc1b164443a84fc12ef85577b2e148
Saqib Rokadia <saqib@saqibr.com>
2023-12-09 12:52:04 -0800

    Used LCM to solve.

--- 2023/Day8/part2.py
+++ 2023/Day8/part2.py
@@ -2,6 +2,9 @@
 
 import re
 import sys
+import math
+from operator import mul
+from functools import reduce
 
 def main() -> None:
     input_file_name = sys.argv[1]
@@ -14,30 +17,40 @@ def main() -> None:
     nav_lookup = dict(map(parse_line, lines[1:]))
     index = 0
     ghost_nodes = list(filter(lambda k: k.endswith("A"), nav_lookup.keys()))
-    index_counts = [None] * len(ghost_nodes)
-    while len(list(filter(lambda i: i is None, index_counts))) > 0 and index < 1000000000:
+    first_index_counts = [None] * len(ghost_nodes)
+    first_loop_index_counts = [None] * len(ghost_nodes)
+    while len(list(filter(lambda i: i is None, first_loop_index_counts))) > 0 and index < 1000000000:
         index_to_use = nav_indexing[index % len(nav_indexing)]
         ghost_nodes = list(map(lambda n: nav_lookup[n][index_to_use], ghost_nodes))
         index += 1
         for i in range(len(ghost_nodes)):
-            if index_counts[i] is None and ghost_nodes[i].endswith("Z"):
-                index_counts[i] = index
+            if first_index_counts[i] is None and ghost_nodes[i].endswith("Z"):
+                first_index_counts[i] = index
+            elif first_loop_index_counts[i] is None and ghost_nodes[i].endswith("Z"):
+                first_loop_index_counts[i] = index
 
-    print(f'index_counts: {index_counts}')
-    max_iterations = max(index_counts)
+    print(f'first_index_counts: {first_index_counts}')
+    print(f'first_loop_index_counts: {first_loop_index_counts}')
+    print(f'list(map(lambda x: x * 2, first_index_counts)): {list(map(lambda x: x * 2, first_index_counts))}')
+    iteration_counts = list(map(lambda idx: first_loop_index_counts[idx] - first_index_counts[idx], range(len(first_index_counts))))
+    max_iterations = max(iteration_counts)
+    max_iterations_index = next(filter(lambda idx: iteration_counts[idx] == max_iterations, range(len(first_index_counts))))
 
-    multiplier = 1.0
-    while not all(map(lambda ic: ((max_iterations * multiplier) % float(ic)) == 0, index_counts)) and multiplier < 1000000000:
-        multiplier += 1.0
-
-    print(f'multiplier: {multiplier}')
-    print(f'max_iterations: {max_iterations}')
-    print(f'max_iterations * multiplier: {max_iterations * multiplier}')
+    multiplier = 0.0
+    lcm = lcmm(*iteration_counts)
+    print(f'lcm: {lcm}')
 
 nav_map = {
     "L": 0,
     "R": 1,
 }
+
+def lcm(a: int, b: int) -> int:
+    gcd = math.gcd(a, b)
+    return abs(a * b) // gcd
+
+def lcmm(*args):
+    return reduce(lcm, args)
 
 def nav_flow(line: str) -> list[int]:
     return list(map(lambda c: nav_map[c], line))
